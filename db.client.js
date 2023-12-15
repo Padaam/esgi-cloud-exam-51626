eb9cfa9beb82a5793364d0e9bf7419052d0f6819
Adam <benhassou.adam@gmail.com>
2023-12-15 11:07:22 +0100

    database changes

--- db.client.js
+++ db.client.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
+  `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`, 
   {
     dialectOptions: {
       ssl: {
